4dbd13f90116bf92f48e4a8fc8a46e031690d498
namazovanton <namazovanton@gmail.com>
2023-03-06 00:22:41 +0300

    Build step

--- app.py
+++ app.py
@@ -1,38 +1,25 @@
-import os
-from flask import request, jsonify
 from flask import Flask
-from marshmallow import ValidationError
 
-from make_query import build_query
-from model import PackageRequestSchema
-
-app = Flask(__name__)
-
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# DATA_DIR = os.path.join(BASE_DIR, "data")
-file_name = "data/apache_logs.txt"
+from db import db
+from views import main_bp
 
 
-@app.route("/perform_query", methods=['POST'])
-def perform_query():
-    query_data = request.json
-    try:
-        validated_data = PackageRequestSchema().load(query_data)
-
-    except ValidationError as error:
-        return jsonify(error.messages), 400
-
-    result = None
-    for query in validated_data['queries']:
-        result = build_query(
-            cmd=query['cmd'],
-            value=query['value'],
-            file_name=validated_data['file_name'],
-            data=result
-        )
-
-    return jsonify(result)
+def create_app(config) -> Flask:
+    app = Flask(__name__)
+    app.config.from_object(config)
+    db.init_app(app)
+    app.register_blueprint(main_bp)
+    # configure_app(application)
+    return app
 
 
-if __name__ == '__main__':
-    app.run(host="localhost", port=10001, debug=True)
+# def configure_app(application: Flask):
+#     db.init_app(application)
+#     api = Api(application)
+#     api.add_namespace(director_ns)
+#     api.add_namespace(genre_ns)
+#     api.add_namespace(movie_ns)
+#     api.add_namespace(user_ns)
+#     api.add_namespace(auth_ns)
+#     with application.app_context():
+#         db.create_all()
